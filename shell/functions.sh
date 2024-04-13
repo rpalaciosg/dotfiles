6154cc79471cdafff1ead227bed2075c92183809
Richard Palacios <apalaciosg91@gmail.com>
2024-04-13 04:17:42 -0500

    Delete scripts

--- shell/functions.sh
+++ shell/functions.sh
@@ -1,19 +1,33 @@
 function cdd() {
-  cd "$(ls -d -- */ | fzf)" || echo "Invalid directory"
+	cd "$(ls -d -- */ | fzf)" || echo "Invalid directory"
 }
 
 function j() {
-  fname=$(declare -f -F _z)
+	fname=$(declare -f -F _z)
 
-  [ -n "$fname" ] || source "$DOTLY_PATH/modules/z/z.sh"
+	[ -n "$fname" ] || source "$DOTLY_PATH/modules/z/z.sh"
 
-  _z "$1"
+	_z "$1"
 }
 
 function recent_dirs() {
-  # This script depends on pushd. It works better with autopush enabled in ZSH
-  escaped_home=$(echo $HOME | sed 's/\//\\\//g')
-  selected=$(dirs -p | sort -u | fzf)
+	# This script depends on pushd. It works better with autopush enabled in ZSH
+	escaped_home=$(echo $HOME | sed 's/\//\\\//g')
+	selected=$(dirs -p | sort -u | fzf)
 
-  cd "$(echo "$selected" | sed "s/\~/$escaped_home/")" || echo "Invalid directory"
+	cd "$(echo "$selected" | sed "s/\~/$escaped_home/")" || echo "Invalid directory"
+}
+
+function zsh_stats() {
+	fc -l 1 | awk '{CMD[$2]++;count++;} END { for (a in CMD)	print CMD[a] " " CMD[a]/count*100 "% " a;}' | grep -v "./" | column -c3 -s " " -t | sort -nr | nl | head -n25
+}
+
+function gc {
+	git add -A
+
+	if [ -z "$1" ]; then
+		git commit - S
+	else
+		git commit -S -m"$1"
+	fi
 }
--- shell/zsh/key-bindings.zsh
+++ shell/zsh/key-bindings.zsh
@@ -1,1 +1,10 @@
 # All bindings can be found https://www.zsh.org/mla/users/2014/msg00266.html
+
+_reverse_search(){
+	# local selected_command=$(fc -rl 1 | awk '{$1="";print substr($0,2)}' | fzf)
+	local selected_command=$(fc -rl 1 | awk '{$1="";print substr($0,2)}' | awk '!seen[$0]++' | fzf)
+  LBUFFER=$selected_command
+}
+
+zle -N  _reverse_search
+bindkey '^r'  _reverse_search
