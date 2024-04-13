f976678d598d9a9ba161004b6d402f873d565be2
Richard Palacios <apalaciosg91@gmail.com>
2024-04-13 04:23:03 -0500

    feat(keybindings): Add reverse_searach binding

--- shell/zsh/key-bindings.zsh
+++ shell/zsh/key-bindings.zsh
@@ -1,5 +1,7 @@
 # All bindings can be found https://www.zsh.org/mla/users/2014/msg00266.html
 
+# Reverse_search keybinding
+
 _reverse_search(){
 	# local selected_command=$(fc -rl 1 | awk '{$1="";print substr($0,2)}' | fzf)
 	local selected_command=$(fc -rl 1 | awk '{$1="";print substr($0,2)}' | awk '!seen[$0]++' | fzf)
