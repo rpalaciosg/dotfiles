53c6c78257a61999ddf0e1e58ec2357888399845
Richard Palacios <apalaciosg91@gmail.com>
2024-04-14 15:11:42 -0500

    feat(key-bindings): add new key-bindings
    - Add `ctrl + k` : change directory to project and open with vscode
    - Add `ctrl + b` : change of git branch

--- shell/zsh/key-bindings.zsh
+++ shell/zsh/key-bindings.zsh
@@ -4,11 +4,31 @@
 
 _reverse_search(){
 	# local selected_command=$(fc -rl 1 | awk '{$1="";print substr($0,2)}' | fzf)
-	# local selected_command=$(fc -rl 1 | awk '{$1="";print substr($0,2)}' | awk '!seen[$0]++' | fzf)
 	local selected_command=$(fc -rl 1 | awk '{$1="";print substr($0,2)}' | awk '!seen[$0]++' | peco)
-	# local selected_command=$(fc -rl 1 | awk '{$1="";print substr($0,2)}' | awk '!seen[$0]++' | peco --selection-prefix →)
   LBUFFER=$selected_command
 }
 
 zle -N  _reverse_search
 bindkey '^r'  _reverse_search
+
+# Abrir proyectos en vscode
+_open_project(){
+  local project=$(ls $HOME/Projects | peco)
+  if [ ! -z $project ]
+  then
+    cd "$HOME/Projects/$project"
+    code .
+  fi
+}
+
+zle -N _open_project
+bindkey "^k" _open_project
+
+# Cambiarme de rama de git
+_change_branch() {
+  local branch_name=$(git branch | peco);
+  git checkout "${branch_name##*( )}";
+}
+
+zle -N _change_branch
+bindkey "^b" _change_branch
