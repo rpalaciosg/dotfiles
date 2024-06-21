3beba9a87695946f3cd7da99a7b5d149c8506fa7
Richard Palacios <apalaciosg91@gmail.com>
2024-06-21 03:10:17 -0500

    feat(nvim): update plugins conf
    - Add conf of neotree.lua
    - Add conf of vim-navigator.lua

--- editors/nvim/lua/plugins/example.lua
+++ editors/nvim/lua/plugins/example.lua
@@ -242,4 +242,14 @@ return {
       })
     end,
   },
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+      },
+    },
+  },
 }
