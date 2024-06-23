015327f1e96028858cdbb8d5b42485761aa9a0c0
Richard Palacios <apalaciosg91@gmail.com>
2024-06-23 17:23:40 -0500

    feat(yazi): add yazi terminal file manager
    Add `yy()` function for call `yazi`
     Changes to be committed:
    	modified:   shell/functions.sh

--- shell/functions.sh
+++ shell/functions.sh
@@ -31,3 +31,11 @@ function gc {
 		git commit -S -m"$1"
 	fi
 }
+function yy() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		cd -- "$cwd"
+	fi
+	rm -f -- "$tmp"
+}
