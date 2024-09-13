5f8a07ba40e18eaa85995563ae1962e9045a362a
Richard Palacios <apalaciosg91@gmail.com>
2024-09-13 03:29:29 -0500

    feat: add editor and zen mode
    - add symlink editor

--- editors/nvim/lua/plugins/ui.lua
+++ editors/nvim/lua/plugins/ui.lua
@@ -155,4 +155,17 @@ return {
 		end,
 	},
 
+  {
+		"folke/zen-mode.nvim",
+		cmd = "ZenMode",
+		opts = {
+			plugins = {
+				gitsigns = true,
+				tmux = true,
+				kitty = { enabled = false, font = "+2" },
+			},
+		},
+		keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
+	},
+
 }
