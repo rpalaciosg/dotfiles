1af4f5d80ca450294ea4c271130383ff8837bcaf
Richard Palacios <apalaciosg91@gmail.com>
2024-09-13 03:49:28 -0500

    feat(ui): add buffer and status line

--- editors/nvim/lua/plugins/ui.lua
+++ editors/nvim/lua/plugins/ui.lua
@@ -124,6 +124,23 @@ return {
 			}
 		end,
 	},
+  -- buffer line
+	{
+		"akinsho/bufferline.nvim",
+		event = "VeryLazy",
+		keys = {
+			{ "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
+			{ "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
+		},
+		opts = {
+			options = {
+				mode = "tabs",
+				-- separator_style = "slant",
+				show_buffer_close_icons = false,
+				show_close_icon = false,
+			},
+		},
+	},
 
   -- filename
 	{
@@ -152,6 +169,25 @@ return {
 					return { { icon, guifg = color }, { " " }, { filename } }
 				end,
 			})
+		end,
+	},
+
+  -- statusline
+	{
+		"nvim-lualine/lualine.nvim",
+		opts = function(_, opts)
+			local LazyVim = require("lazyvim.util")
+			opts.sections.lualine_c[4] = {
+				LazyVim.lualine.pretty_path({
+					length = 0,
+					relative = "cwd",
+					modified_hl = "MatchParen",
+					directory_hl = "",
+					filename_hl = "Bold",
+					modified_sign = "",
+					readonly_icon = " 󰌾 ",
+				}),
+			}
 		end,
 	},
 
