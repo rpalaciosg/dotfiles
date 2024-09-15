91de7f0653c6e43c3c5c9d6429b78ed203c7ccef
Richard Palacios <apalaciosg91@gmail.com>
2024-09-14 23:59:01 -0500

    feat(nvim): update coding inc-rename

--- editors/nvim/lua/plugins/coding.lua
+++ editors/nvim/lua/plugins/coding.lua
@@ -5,4 +5,41 @@ return {
 		cmd = "IncRename",
 		config = true,
 	},
+
+  -- conform
+-- {
+--   "stevearc/conform.nvim",
+--   opts = {
+--     formatters_by_ft = {
+--       lua = { "stylua" },
+--       python = { "isort", "black" },
+--       javascript = { "prettier" },
+--       typescript = { "prettier" },
+--       javascriptreact = { "prettier" },
+--       typescriptreact = { "prettier" },
+--       css = { "prettier" },
+--       html = { "prettier" },
+--       json = { "prettier" },
+--       yaml = { "prettier" },
+--       markdown = { "prettier" },
+--       ["markdown.mdx"] = { "prettier" },
+--       graphql = { "prettier" },
+--       handlebars = { "prettier" },
+--       cs = { "csharpier" },
+--       make = { "checkmake" },
+--       dockerfile = { "hadolint" },
+--     },
+--     format_on_save = {
+--       lsp_fallback = true,
+--       async = false,
+--       timeout_ms = 500,
+--     },
+--     log_level = vim.log.levels.ERROR,
+--     notify_on_error = true,
+--   },
+--   config = function(_, opts)
+--     require("conform").setup(opts)
+--     vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+--   end,
+-- }
 }
