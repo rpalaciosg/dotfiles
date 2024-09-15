c2334194c45a5a584397f862ead65e60f9f8d01b
Richard Palacios <apalaciosg91@gmail.com>
2024-09-15 00:11:02 -0500

    fix: typo in lsp

--- editors/nvim/lua/plugins/lsp.lua
+++ editors/nvim/lua/plugins/lsp.lua
@@ -1,7 +1,8 @@
 return {
   {"williamboman/mason.nvim",
     opts = function(_, opts)
-      vim.list.extend(opts.ensure_installed, {
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, {
         "stylua",
         "prettier",
         "selene",
