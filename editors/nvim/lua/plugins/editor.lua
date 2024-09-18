ca33209c15aafb9152a731f38106dc5c031707ae
Richard Palacios <apalaciosg91@gmail.com>
2024-09-17 23:42:00 -0500

    feat(nvim): add editor plugins

--- editors/nvim/lua/plugins/editor.lua
+++ editors/nvim/lua/plugins/editor.lua
@@ -34,4 +34,17 @@ return {
 			},
 		},
 	},
+
+  {
+		"dinhhuy258/git.nvim",
+		event = "BufReadPre",
+		opts = {
+			keymaps = {
+				-- Open blame window
+				blame = "<Leader>gb",
+				-- Open file/folder in git repository
+				browse = "<Leader>go",
+			},
+		},
+	},
 }
