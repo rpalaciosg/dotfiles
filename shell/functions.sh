7d37d2304f696d677eb269ed43b42b4a95150fcd
Richard Palacios <apalaciosg91@gmail.com>
2024-10-06 09:13:38 -0500

    feat(functions): add tmuxas for select attach tmux session

--- shell/functions.sh
+++ shell/functions.sh
@@ -1,42 +1,56 @@
 function cdd() {
-	cd "$(ls -d -- */ | fzf)" || echo "Invalid directory"
+  cd "$(ls -d -- */ | fzf)" || echo "Invalid directory"
 }
 
 function j() {
-	fname=$(declare -f -F _z)
+  fname=$(declare -f -F _z)
 
-	[ -n "$fname" ] || source "$DOTLY_PATH/modules/z/z.sh"
+  [ -n "$fname" ] || source "$DOTLY_PATH/modules/z/z.sh"
 
-	_z "$1"
+  _z "$1"
 }
 
 function recent_dirs() {
-	# This script depends on pushd. It works better with autopush enabled in ZSH
-	escaped_home=$(echo $HOME | sed 's/\//\\\//g')
-	selected=$(dirs -p | sort -u | fzf)
+  # This script depends on pushd. It works better with autopush enabled in ZSH
+  escaped_home=$(echo $HOME | sed 's/\//\\\//g')
+  selected=$(dirs -p | sort -u | fzf)
 
-	cd "$(echo "$selected" | sed "s/\~/$escaped_home/")" || echo "Invalid directory"
+  cd "$(echo "$selected" | sed "s/\~/$escaped_home/")" || echo "Invalid directory"
 }
 
 function zsh_stats() {
-	fc -l 1 | awk '{CMD[$2]++;count++;} END { for (a in CMD)	print CMD[a] " " CMD[a]/count*100 "% " a;}' | grep -v "./" | column -c3 -s " " -t | sort -nr | nl | head -n25
+  fc -l 1 | awk '{CMD[$2]++;count++;} END { for (a in CMD)	print CMD[a] " " CMD[a]/count*100 "% " a;}' | grep -v "./" | column -c3 -s " " -t | sort -nr | nl | head -n25
 }
 
 function gc {
-	git add -A
+  git add -A
 
-	if [ -z "$1" ]; then
-		git commit - S
-	else
-		git commit -S -m"$1"
-	fi
+  if [ -z "$1" ]; then
+    git commit - S
+  else
+    git commit -S -m"$1"
+  fi
 }
 
 function yy() {
-	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
-	yazi "$@" --cwd-file="$tmp"
-	if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
-		cd -- "$cwd"
-	fi
-	rm -f -- "$tmp"
+  local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
+  yazi "$@" --cwd-file="$tmp"
+  if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+    cd -- "$cwd"
+  fi
+  rm -f -- "$tmp"
+}
+
+function tmuxas() {
+  # Listar sesiones de tmux y pasarlas a fzf
+  session=$(tmux ls | awk '{print $1}' | fzf --height 40% --reverse)
+
+  # Comprobar si se seleccionó una sesión
+  if [ -n "$session" ]; then
+    # Adjuntar a la sesión seleccionada
+    tmux attach-session -t "$session"
+  else
+    echo "No se seleccionó ninguna sesión."
+  fi
+
 }
