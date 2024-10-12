7d02d3eb3da6a88c9b8914034f7cf16006a93a8e
Richard Palacios <apalaciosg91@gmail.com>
2024-10-11 23:30:54 -0500

    feat(nvim): update ui and plugins
    - Update Noice
    - Update lualine
    - Update statusline
    - Update whichkey
    - Update incline

--- editors/nvim/lua/plugins/ui.lua
+++ editors/nvim/lua/plugins/ui.lua
@@ -1,11 +1,11 @@
-local mocha = require "catppuccin.palettes.mocha"
+local mocha = require("catppuccin.palettes.mocha")
 return {
-{
-  "nvimdev/dashboard-nvim",
-  event = "VimEnter",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = function(_, opts)
-   local logo = [[
+  {
+    "nvimdev/dashboard-nvim",
+    event = "VimEnter",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = function(_, opts)
+      local logo = [[
                                                                        
          ████ ██████           █████      ██                     
         ███████████             █████                             
@@ -15,215 +15,243 @@ return {
     ███████████ ███    ███ █████████ █████ █████ ████ █████  
    ██████  █████████████████████ ████ █████ █████ ████ ██████ 
    ]]
-    logo = string.rep("\n", 5) .. logo .. "\n\n"
-    opts.config.header = vim.split(logo, "\n")
-    opts.config.theme= 'doom'
-  end,
-	},
--- neo-tree modified
-{
- "nvim-neo-tree/neo-tree.nvim",
-   dependencies = {
-   "nvim-lua/plenary.nvim",
-   "nvim-tree/nvim-web-devicons",
-   "MunifTanjim/nui.nvim",
-   },
- opts = {
-     popup_border_style = "rounded",
-     enable_git_status = true,
-     enable_diagnostics = true,
-   window = {
-     mappings = {
-       ["<space>"] = "none",
-     },
-   },
-   filesystem = {
-     filtered_items = {
-       visible = true,
-       hide_dotfiles = false,
-       hide_gitignored = false,
-     },
-   },
-   event_handlers = {
-     {
-       event = "neo_tree_buffer_enter",
-       handler = function()
-         vim.opt_local.relativenumber = true
-       end,
-     },
-   },
- },
-},
+      logo = string.rep("\n", 5) .. logo .. "\n\n"
+      opts.config.header = vim.split(logo, "\n")
+      opts.config.theme = "doom"
+    end,
+  },
+  -- neo-tree modified
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    },
+    opts = {
+      popup_border_style = "rounded",
+      enable_git_status = true,
+      enable_diagnostics = true,
+      window = {
+        mappings = {
+          ["<space>"] = "none",
+        },
+      },
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+      },
+      event_handlers = {
+        {
+          event = "neo_tree_buffer_enter",
+          handler = function()
+            vim.opt_local.relativenumber = true
+          end,
+        },
+      },
+    },
+  },
 
   -- messages, cmdline and the popupmenu
-	{
-		"folke/noice.nvim",
-		opts = function(_, opts)
-			table.insert(opts.routes, {
-				filter = {
-					event = "notify",
-					find = "No information available",
-				},
-				opts = { skip = true },
-			})
-			local focused = true
-			vim.api.nvim_create_autocmd("FocusGained", {
-				callback = function()
-					focused = true
-				end,
-			})
-			vim.api.nvim_create_autocmd("FocusLost", {
-				callback = function()
-					focused = false
-				end,
-			})
-			table.insert(opts.routes, 1, {
-				filter = {
-					cond = function()
-						return not focused
-					end,
-				},
-				view = "notify_send",
-				opts = { stop = false },
-			})
-
-			opts.commands = {
-				all = {
-					-- options for the message history that you get with `:Noice`
-					view = "split",
-					opts = { enter = true, format = "details" },
-					filter = {},
-				},
-			}
-
-			vim.api.nvim_create_autocmd("FileType", {
-				pattern = "markdown",
-				callback = function(event)
-					vim.schedule(function()
-						require("noice.text.markdown").keys(event.buf)
-					end)
-				end,
-			})
-
-			opts.presets.lsp_doc_border = true
-		end,
-	},
   {
-      "rcarriga/nvim-notify",
-      opts = {
-        timeout = 5000,
-      },
+    "folke/noice.nvim",
+    opts = function(_, opts)
+      table.insert(opts.routes, {
+        filter = {
+          event = "notify",
+          find = "No information available",
+        },
+        opts = { skip = true },
+      })
+      local focused = true
+      vim.api.nvim_create_autocmd("FocusGained", {
+        callback = function()
+          focused = true
+        end,
+      })
+      vim.api.nvim_create_autocmd("FocusLost", {
+        callback = function()
+          focused = false
+        end,
+      })
+      table.insert(opts.routes, 1, {
+        filter = {
+          cond = function()
+            return not focused
+          end,
+        },
+        view = "notify_send",
+        opts = { stop = false },
+      })
+
+      opts.commands = {
+        all = {
+          -- options for the message history that you get with `:Noice`
+          view = "split",
+          opts = { enter = true, format = "details" },
+          filter = {},
+        },
+      }
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "markdown",
+        callback = function(event)
+          vim.schedule(function()
+            require("noice.text.markdown").keys(event.buf)
+          end)
+        end,
+      })
+
+      opts.presets.lsp_doc_border = true
+    end,
+  },
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      timeout = 5000,
+    },
   },
   -- animations
-	{
-		"echasnovski/mini.animate",
-		event = "VeryLazy",
-		opts = function(_, opts)
-			opts.scroll = {
-				enable = false,
-			}
-		end,
-	},
+  {
+    "echasnovski/mini.animate",
+    event = "VeryLazy",
+    opts = function(_, opts)
+      opts.scroll = {
+        enable = false,
+      }
+    end,
+  },
   -- buffer line
-	{
-		"akinsho/bufferline.nvim",
-    dependencies = {"nvim-tree/nvim-web-devicons"},
-		event = "VeryLazy",
+  {
+    "akinsho/bufferline.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    event = "VeryLazy",
     version = "*",
-		keys = {
-			{ "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
-			{ "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
-		},
-		opts = {
-			options = {
-				mode = "tabs",
-				separator_style = "slant",
-				show_buffer_close_icons = false,
-				show_close_icon = false,
-			},
-		},
-	},
+    keys = {
+      { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
+      { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
+    },
+    opts = {
+      options = {
+        mode = "tabs",
+        separator_style = "slant",
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+      },
+    },
+  },
 
   -- filename
-	{
-		"b0o/incline.nvim",
-		event = "BufReadPre",
-		priority = 1000,
-		config = function()
+  {
+    "b0o/incline.nvim",
+    event = "BufReadPre",
+    priority = 1000,
+    config = function()
       local colors = require("catppuccin.palettes").get_palette()
       require("incline").setup({
-				highlight = {
-					groups = {
-          InclineNormal = { guibg = colors.pink, guifg = colors.base },
-          InclineNormalNC = { guibg = colors.surface0, guifg = colors.subtext0 },
-					},
-				},
-				window = { margin = { vertical = 0, horizontal = 1 } },
-				hide = {
-					cursorline = true,
-				},
+        highlight = {
+          groups = {
+            InclineNormal = { guibg = colors.pink, guifg = colors.base },
+            InclineNormalNC = { guibg = colors.surface0, guifg = colors.subtext0 },
+          },
+        },
+        window = { margin = { vertical = 0, horizontal = 1 } },
+        hide = {
+          cursorline = true,
+        },
 
         render = function(props)
-        local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
-        local icon, icon_color = require("nvim-web-devicons").get_icon_color(filename)
-        local modified = vim.bo[props.buf].modified
-        local fg = props.focused and colors.base or colors.subtext0
-        local bg = props.focused and colors.pink or colors.surface0
+          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+          local icon, icon_color = require("nvim-web-devicons").get_icon_color(filename)
+          local modified = vim.bo[props.buf].modified
+          local fg = props.focused and colors.base or colors.subtext0
+          local bg = props.focused and colors.pink or colors.surface0
 
-        local mod_color
-        if modified then
-          mod_color = props.focused and colors.peach or colors.maroon
-        else
-          mod_color = bg
-        end
+          local mod_color
+          if modified then
+            mod_color = props.focused and colors.peach or colors.maroon
+          else
+            mod_color = bg
+          end
 
-				if vim.bo[props.buf].modified then
-					filename = "[+] " .. filename
-					-- filename = filename .. " [+]"
-				end
+          if vim.bo[props.buf].modified then
+            filename = "[+] " .. filename
+            -- filename = filename .. " [+]"
+          end
 
-        return {
-          { icon, guifg = icon_color, guibg = bg },
-          { " ", guibg = bg },
-          { filename, gui = modified and "bold,italic" or "bold", guifg = fg, guibg = bg },
-          { modified and " ●" or "  ", guifg = mod_color, guibg = bg },
-
-        }
-      end,
-			})
-		end,
-	},
+          return {
+            { icon, guifg = icon_color, guibg = bg },
+            { " ", guibg = bg },
+            { filename, gui = modified and "bold,italic" or "bold", guifg = fg, guibg = bg },
+            { modified and " ●" or "  ", guifg = mod_color, guibg = bg },
+          }
+        end,
+      })
+    end,
+  },
 
   -- statusline
-	{
-		"nvim-lualine/lualine.nvim",
-    dependencies = {"nvim-tree/nvim-web-devicons"},
-		opts = function(_, opts)
-			local LazyVim = require("lazyvim.util")
-			opts.sections.lualine_c[4] = {
-				LazyVim.lualine.pretty_path({
-					length = 0,
-					relative = "cwd",
-					modified_hl = "MatchParen",
-					directory_hl = "",
-					filename_hl = "Bold",
-					modified_sign = "",
-					readonly_icon = " 󰌾 ",
-				}),
-			}
-		end,
-	},
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = function(_, opts)
+      local LazyVim = require("lazyvim.util")
+      opts.sections.lualine_c[4] = {
+        LazyVim.lualine.pretty_path({
+          length = 0,
+          relative = "cwd",
+          modified_hl = "MatchParen",
+          directory_hl = "",
+          filename_hl = "Bold",
+          modified_sign = "",
+          readonly_icon = " 󰌾 ",
+        }),
+      }
+    end,
+  },
 
   {
-		"folke/zen-mode.nvim",
-		cmd = "ZenMode",
-		opts = {
-			plugins = {
-				gitsigns = true,
-				tmux = true,
-				kitty = { enabled = false, font = "+2" },
-			},
-		},
-		keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
-	},
+    "folke/zen-mode.nvim",
+    cmd = "ZenMode",
+    opts = {
+      plugins = {
+        gitsigns = true,
+        tmux = true,
+        kitty = { enabled = false, font = "+2" },
+      },
+    },
+    keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
+  },
+  -- which-key
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 500
+    end,
+    opts = {
+      preset = "modern", -- Cambiar a "modern"
+      layout = {
+        height = { min = 4, max = 25 }, -- Altura mínima y máxima de las columnas
+        width = { min = 20, max = 50 }, -- Ancho mínimo y máximo de las columnas
+        spacing = 3, -- Espaciado entre columnas
+        align = "center", -- Alineación de las columnas
+      },
+      window = {
+        border = "rounded", -- Tipo de borde (ninguno, sencillo, doble, sombreado, redondeado)
+        position = "center", -- Posición de la ventana
+        margin = { 1, 0, 1, 0 }, -- Margen extra de la ventana
+        padding = { 2, 2, 2, 2 }, -- Relleno extra de la ventana
+        winblend = 10, -- Transparencia
+      },
+      ignore_missing = true,
+      show_help = true,
+      show_keys = true,
+      triggers = "auto",
+    },
+  },
 }
