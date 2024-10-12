6c4d45f73323e3d79692d26f6c1796c62d1b0dbf
Richard Palacios <apalaciosg91@gmail.com>
2024-10-11 23:34:37 -0500

    feat(nvim): update autocmds for nvim

--- editors/nvim/lua/config/autocmds.lua
+++ editors/nvim/lua/config/autocmds.lua
@@ -3,15 +3,41 @@
 -- Add any additional autocmds here
 -- Turn off paste mode when leaving insert
 vim.api.nvim_create_autocmd("InsertLeave", {
-	pattern = "*",
-	command = "set nopaste",
+  pattern = "*",
+  command = "set nopaste",
 })
 
 -- Disable the concealing in some file formats
 -- The default conceallevel is 3 in LazyVim
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "json", "jsonc", "markdown" },
-	callback = function()
-		vim.opt.conceallevel = 0
-	end,
+  pattern = { "json", "jsonc", "markdown" },
+  callback = function()
+    vim.opt.conceallevel = 0
+  end,
+})
+
+-- Config colorcolumn
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "lua",
+  callback = function()
+    vim.opt_local.colorcolumn = "120"
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+  callback = function()
+    vim.api.nvim_create_autocmd("CursorMoved", {
+      buffer = 0,
+      callback = function()
+        local max_line_length = 88
+        local current_line = vim.api.nvim_get_current_line()
+        if #current_line > max_line_length - 15 then
+          vim.opt_local.colorcolumn = tostring(max_line_length)
+        else
+          vim.opt_local.colorcolumn = ""
+        end
+      end,
+    })
+  end,
 })
